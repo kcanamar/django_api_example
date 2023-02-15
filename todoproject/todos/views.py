4b3ff92bde94890d4d3c9d272501d55771562fd0
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-14 17:23:50 -0800

    creates api

--- todoproject/todos/views.py
+++ todoproject/todos/views.py
@@ -7,5 +7,5 @@ from .serializers import TodoSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
-    serialzer_class = TodoSerializer
+    serializer_class = TodoSerializer
     permission_classes = [permissions.AllowAny]
